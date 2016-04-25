5059a6c73cbfc485e442982a95736d1ae79dcc15
Uniquers <Uniquers@853a975a-7e59-4707-9df1-4264b926ceb7>
2016-04-25 09:47:35 +0000

    1、补充遗忘提交的调色板和IP地址控件类

---
+++ Library/DuiLib/Control/UIIPAddress.cpp
@@ -0,0 +1,223 @@
+#include "stdafx.h"
+
+namespace DuiLib
+{
+	CIPAddressUI::CIPAddressUI()
+		:m_nButtonState(0)
+	{
+		for (int n=0; n<ADDRESS_IPV4; ++n)
+		{
+			m_pBlock[n] = new CRichEditUI;
+			m_pBlock[n]->SetAttribute(_T("align"), _T("center"));
+			m_pBlock[n]->SetAttribute(_T("numberonly"), _T("true"));
+			m_pBlock[n]->SetAttribute(_T("rich"), _T("false"));
+			m_pBlock[n]->SetAttribute(_T("multiline"), _T("false"));
+			m_pBlock[n]->SetAttribute(_T("maxchar"), _T("3")); 
+			m_pBlock[n]->SetAttribute(_T("font"), _T("0"));
+			m_pBlock[n]->OnEvent += MakeDelegate(this, &CIPAddressUI::DoEvent);
+		}
+		//1一个IP地址控件，由4个编辑框和3个小黑点组成
+		for (int n=0; n<7; ++n)
+		{
+			if (n%2 == 0)
+				Add(m_pBlock[n/2]);
+			else
+			{
+				CLabelUI* pDot = new CLabelUI;
+				//pDot->OnEvent += MakeDelegate(this, &CIPAddressUI::DoEvent);
+				pDot->SetText(_T("."));
+				pDot->SetAutoCalcWidth(true);
+				Add(pDot);
+			}
+		}
+	}
+
+	LPCTSTR CIPAddressUI::GetClass() const
+	{
+		return _T("IPAddressUI");
+	}
+
+	LPVOID CIPAddressUI::GetInterface(LPCTSTR pstrName)
+	{
+		if (_tcsicmp(pstrName, DUI_CTR_IPADDRESS) == 0 )	return static_cast<CIPAddressUI*>(this);
+		else
+			return CHorizontalLayoutUI::GetInterface(pstrName);
+	}
+
+	void CIPAddressUI::DoEvent(TEventUI& event)
+	{
+		if( !IsMouseEnabled() && event.Type > UIEVENT__MOUSEBEGIN && event.Type < UIEVENT__MOUSEEND ) {
+			if( m_pParent != NULL ) m_pParent->DoEvent(event);
+			else CHorizontalLayoutUI::DoEvent(event);
+			return;
+		}
+
+		if( event.Type == UIEVENT_MOUSEENTER ){
+			if( IsEnabled() ) {
+				m_nButtonState |= UISTATE_HOT;
+				Invalidate();
+			}
+			return;
+		}else if( event.Type == UIEVENT_MOUSELEAVE ){
+			if( IsEnabled() ) {
+				m_nButtonState &= ~UISTATE_HOT;
+				Invalidate();
+			}
+			return;
+		}
+
+		CHorizontalLayoutUI::DoEvent(event);
+	}
+
+	bool CIPAddressUI::DoEvent(LPVOID lParam)
+	{
+		const TEventUI* pEvent = (TEventUI*)lParam;
+		if (pEvent->Type == UIEVENT_CHAR && pEvent->chKey == _T('.')){
+			if (IsEnabled()){
+				//如果是最后一个输入框，输入'.'就不用跳转了
+				if (pEvent->pSender == m_pBlock[3])
+					return false;
+				
+				pEvent->pSender->GetManager()->SetNextTabControl();
+				CRichEditUI* pRichedit = (CRichEditUI*)GetManager()->GetFocus();
+				//默认选中编辑框内容
+				if (pRichedit)
+					pRichedit->SetSel(0,pRichedit->GetTextLength());
+
+				//由于切换焦点后，当前控件会失去焦点，故手动设置焦点
+				m_bFocused = true;
+				return false;
+			}
+		}
+		//WM_KEYDOWN RichEdit控件默认没有转发，需要手动转发
+		else if (pEvent->Type == UIEVENT_KEYDOWN)	
+		{	
+			if (IsEnabled()){
+				if (pEvent->chKey != VK_LEFT && pEvent->chKey != VK_RIGHT)
+					return false;
+
+				if ((pEvent->chKey == VK_LEFT && pEvent->pSender == m_pBlock[0]) ||
+					(pEvent->chKey == VK_RIGHT && pEvent->pSender == m_pBlock[3]))
+					return false;
+
+				//判断当前是否需要切换焦点，目前以光标是否在开头或者末尾为准
+				CRichEditUI* pRichedit = (CRichEditUI*)GetManager()->GetFocus();
+				if (pRichedit->GetInterface(DUI_CTR_RICHEDIT) != pRichedit)
+					return false;
+
+				LONG lStart=0, lEnd=0;
+				pRichedit->GetSel(lStart, lEnd);
+				//获取RichEdit文本长度
+				LONG lTextLen = pRichedit->GetTextLength();
+				if ((pEvent->chKey == VK_LEFT && lStart != 0) ||
+					(pEvent->chKey == VK_RIGHT && lEnd != lTextLen))
+					return false;
+
+				pEvent->pSender->GetManager()->SetNextTabControl(pEvent->chKey == VK_RIGHT);
+
+				m_bFocused = true;
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	void CIPAddressUI::PaintStatusImage(HDC hDC)
+	{
+		if( IsFocused() ) m_nButtonState |= UISTATE_FOCUSED;
+		else m_nButtonState &= ~UISTATE_FOCUSED;
+		if( !IsEnabled() ) m_nButtonState |= UISTATE_DISABLED;
+		else m_nButtonState &= ~UISTATE_DISABLED;
+
+		if( (m_nButtonState & UISTATE_DISABLED) != 0 ) {
+			if( !m_sDisabledImage.IsEmpty() ) {
+				if( !DrawImage(hDC, (LPCTSTR)m_sDisabledImage) ) m_sDisabledImage.Empty();
+				else return;
+			}
+		}
+		else if( (m_nButtonState & UISTATE_FOCUSED) != 0 ) {
+			if( !m_sFocusedImage.IsEmpty() ) {
+				if( !DrawImage(hDC, (LPCTSTR)m_sFocusedImage) ) m_sFocusedImage.Empty();
+				else return;
+			}
+		}
+		else if( (m_nButtonState & UISTATE_HOT) != 0 ) {
+			if( !m_sHotImage.IsEmpty() ) {
+				if( !DrawImage(hDC, (LPCTSTR)m_sHotImage) ) m_sHotImage.Empty();
+				else return;
+			}
+		}
+
+		if( !m_sNormalImage.IsEmpty() ) {
+			if( !DrawImage(hDC, (LPCTSTR)m_sNormalImage) ) m_sNormalImage.Empty();
+			else return;
+		}
+	}
+
+	void CIPAddressUI::SetAttribute(LPCTSTR pstrName, LPCTSTR pstrValue)
+	{
+		if (_tcsicmp(pstrName,_T("normalimage")) == 0)	SetNormalImage(pstrValue);
+		else if (_tcsicmp(pstrName,_T("hotimage")) == 0)	SetHotImage(pstrValue);
+		else if (_tcsicmp(pstrName,_T("focusedimage")) == 0)  SetFocusedImage(pstrValue);
+		else if (_tcsicmp(pstrName,_T("disabledimage")) == 0) SetDisabledImage(pstrValue);
+		else
+			CHorizontalLayoutUI::SetAttribute(pstrName, pstrValue);
+	}
+
+	void CIPAddressUI::SetText(LPCTSTR pstrText)
+	{
+		TCHAR szIPByte[4][10] = {0};
+		_stscanf_s(pstrText, _T("%[^.].%[^.].%[^.].%s"), szIPByte[0],_countof(szIPByte[0]),
+																					  szIPByte[1],_countof(szIPByte[1]),			
+																					  szIPByte[2],_countof(szIPByte[2]),
+																					  szIPByte[3],_countof(szIPByte[3]));
+
+
+		for (int n=0; n< ADDRESS_IPV4; ++n)
+			m_pBlock[n]->SetText(szIPByte[n]);
+	}
+	
+	CDuiString CIPAddressUI::GetText()
+	{
+		CDuiString strText;
+		strText.Format(_T("%s.%s.%s.%s"), (LPCTSTR)m_pBlock[0]->GetText(), 
+																(LPCTSTR)m_pBlock[1]->GetText(), 
+																(LPCTSTR)m_pBlock[2]->GetText(), 
+																(LPCTSTR)m_pBlock[3]->GetText());
+
+		return strText;
+	}
+
+	void CIPAddressUI::SetNormalImage(LPCTSTR lpszNormalImage)
+	{
+		if (m_sNormalImage == lpszNormalImage)
+			return;
+		m_sNormalImage = lpszNormalImage;
+		Invalidate();
+	}
+
+	void CIPAddressUI::SetHotImage(LPCTSTR lpszHotImage)
+	{
+		if (m_sHotImage == lpszHotImage)
+			return;
+		m_sHotImage = lpszHotImage;
+		Invalidate();
+	}
+
+	void CIPAddressUI::SetFocusedImage(LPCTSTR lpszFocusedImage)
+	{
+		if (m_sFocusedImage == lpszFocusedImage)
+			return;
+		m_sFocusedImage = lpszFocusedImage;
+		Invalidate();
+	}
+
+	void CIPAddressUI::SetDisabledImage(LPCTSTR lpszDisabledImage)
+	{
+		if (m_sDisabledImage == lpszDisabledImage)
+			return;
+		m_sDisabledImage = lpszDisabledImage;
+		Invalidate();
+	}
+}
---
+++ Library/DuiLib/Control/UIIPAddress.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace DuiLib
+{
+	#define ADDRESS_IPV4		4		//IPv4地址使用的数据个数
+	
+	class UILIB_API CIPAddressUI : public CHorizontalLayoutUI
+	{
+		public:
+			CIPAddressUI();
+			/*~CIPAddressUI();*/
+
+		public:
+			virtual LPCTSTR GetClass() const;
+			virtual LPVOID GetInterface(LPCTSTR pstrName);
+			virtual void DoEvent(TEventUI& event);
+			virtual void SetAttribute(LPCTSTR pstrName, LPCTSTR pstrValue);
+			virtual void SetText(LPCTSTR pstrText);
+			virtual CDuiString CIPAddressUI::GetText();
+			virtual void PaintStatusImage(HDC hDC);
+		protected:
+			bool DoEvent(LPVOID lParam);
+
+			void SetNormalImage(LPCTSTR lpszNormalImage);
+			void SetHotImage(LPCTSTR lpszHotImage);
+			void SetFocusedImage(LPCTSTR lpszFocusedImage);
+			void SetDisabledImage(LPCTSTR lpszDisabledImage);
+
+		protected:
+			CRichEditUI* m_pBlock[ADDRESS_IPV4];
+			UINT m_nButtonState;
+
+			CDuiString m_sNormalImage;			//编辑框普通状态图片
+			CDuiString m_sFocusedImage;			//编辑框获取焦点状态图片
+			CDuiString m_sHotImage;					
+			CDuiString m_sDisabledImage;
+	};
+}
