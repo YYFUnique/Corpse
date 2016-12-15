59bf469b07e5a26b2e537f0be7d30b782f8d5f76
Uniquers <Uniquers@853a975a-7e59-4707-9df1-4264b926ceb7>
2016-12-15 05:50:04 +0000

    1、新增3D旋转演示功能

---
+++ Demo/Installer/3DView.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Image3D/3dTransform.h"
+using namespace IMAGE3D;
+
+class C3DViewUI : public CVerticalLayoutUI
+{
+public:
+	C3DViewUI();
+
+	LPCTSTR GetClass() const;
+	LPVOID GetInterface(LPCTSTR pstrName);
+
+	void DoPaint(HDC hDC,const RECT& rcPaint);
+	void SetAttribute(LPCTSTR pstrName, LPCTSTR pstrValue);
+
+	void StartEffect(BOOL bStart);
+	void Update();
+	void SetRotateX(int nRotateX);
+	void SetRotateY(int nRotateY);
+	void SetRotateZ(int nRotateZ);
+	void SetOffsetZ(int nOffsetZ);
+	void SetBmpOrig(HBITMAP hBmpOrig);
+	PARAM3DTRANSFORM& Get3dParam();
+
+protected:
+	HBITMAP			m_hBmpOrig;
+	HBITMAP			m_hBmpTrans;
+	BOOL				m_bPlay;
+	HDC					m_hMemDC;
+	PARAM3DTRANSFORM	m_3dParam;
+};
--- Demo/Installer/Installer.h
+++ Demo/Installer/Installer.h
@@ -6,21 +6,30 @@ public:
 	CInstaller();
 	~CInstaller();
 public:
+	void OnFinalMessage( HWND hWnd );
 	virtual LPCTSTR    GetWindowClassName() const;
 	virtual CDuiString GetSkinFile();
 	virtual CDuiString GetZIPFileName() const;
 	virtual CDuiString GetSkinFolder();
-	virtual LPCTSTR GetResourceID() const;
+	UILIB_RESOURCETYPE GetResourceType() const;
+	//virtual LPCTSTR GetResourceID() const;
 	virtual void Notify(TNotifyUI& msg);
 	void InitWindow();
 	LRESULT HandleCustomMessage(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
 	CControlUI* CreateControl(LPCTSTR pstrClass);
 
 protected:
-	DUI_DECLARE_MESSAGE_MAP()
+
 	void OnClick(TNotifyUI& msg);
+	void OnTimer(TNotifyUI& msg);
 	virtual LRESULT OnCreate(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
 	void OnMenuItemClick(CControlUI* pControl);
 private:
 	CMenuUI* pMenu;
+
+	int m_nZStep;
+	int m_nYStep;
+	int m_nFrameIndex;
+	HBITMAP m_hBitmapBefore;
+	HBITMAP m_hBitmapAfter;
 };
