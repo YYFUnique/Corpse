e70514f9b8bff18a5d427e96fe75c4cddb4e236e
Uniquers <Uniquers@853a975a-7e59-4707-9df1-4264b926ceb7>
2018-05-29 08:50:21 +0000

    1、新版本NSIS打包工具界面插件

--- Library/nsTBCIASkinEngine/nsTBCIASkinEngine.h
+++ Library/nsTBCIASkinEngine/nsTBCIASkinEngine.h
@@ -9,17 +9,24 @@
 #endif
 
 // 初始化 Duilib 界面库
-NSIS_API void InitTBCIASkinEngine(HWND hwndParent, int string_size, TCHAR* variables, stack_t** stacktop, extra_parameters *extra);
+NSIS_API void InitTBCIASkinEngine(HWND hwndParent, int string_size, LPTSTR variables, stack_t** stacktop, extra_parameters *extra);
 
 // 显示对话框
-NSIS_API void ShowMsgBox(HWND hwndParent, int string_size, TCHAR* variables, stack_t** stacktop, extra_parameters *extra);
+NSIS_API void ShowMsgBox(HWND hwndParent, int string_size, LPTSTR variables, stack_t** stacktop, extra_parameters *extra);
 
-NSIS_API void ShowPage(HWND hwndParent, int nStringSize, TCHAR* lpszVariables, stack_t** pStackTop, extra_parameters *extra);
-NSIS_API void BackPage(HWND hwndParent, int string_size, TCHAR* variables, stack_t** stacktop, extra_parameters *extra);
-NSIS_API void NextPage(HWND hwndParent, int string_size, TCHAR* variables, stack_t** stacktop, extra_parameters *extra);
+NSIS_API void ShowPage(HWND hwndParent, int nStringSize, LPTSTR lpszVariables, stack_t** pStackTop, extra_parameters *extra);
+NSIS_API void BackPage(HWND hwndParent, int string_size, LPTSTR variables, stack_t** stacktop, extra_parameters *extra);
+NSIS_API void NextPage(HWND hwndParent, int string_size, LPTSTR variables, stack_t** stacktop, extra_parameters *extra);
 NSIS_API void SetWindowTitle(HWND hWndParent, int nStringSize, LPTSTR lpszVariables, stack_t** pStackTop, extra_parameters *extra);
 NSIS_API void FindControl(HWND hwndParent, int nStringSize, LPTSTR lpszVariables, stack_t** pStackTop, extra_parameters *extra);
-NSIS_API void GetCheckStatus(HWND hwndParent, int string_size, TCHAR* variables, stack_t** stacktop, extra_parameters *extra);
+NSIS_API void GetCheckStatus(HWND hwndParent, int string_size, LPTSTR variables, stack_t** stacktop, extra_parameters *extra);
 NSIS_API void TBCIAClose(HWND hWndParent, int nStringSize, LPTSTR lpszVariables, stack_t** pStackTop, extra_parameters *extra);
 NSIS_API void ExitTBCIASkinEngine(HWND hWndParent, int nStringSize, LPTSTR lpszVariables, stack_t** pStackTop, extra_parameters *extra);
-NSIS_API void OnControlBindNSISScript(HWND hwndParent, int string_size, char *variables, stack_t **stacktop, extra_parameters *extra);
+NSIS_API void OnControlBindNSISScript(HWND hwndParent, int string_size, LPTSTR variables, stack_t **stacktop, extra_parameters *extra);
+
+
+NSIS_API void SetProcessValue(HWND hwndParent, int string_size, LPTSTR variables, stack_t **stacktop, extra_parameters *extra);
+//NSIS_API void SetProgressChangeCallback(HWND hwndParent, int string_size, LPTSTR variables, stack_t **stacktop, extra_parameters *extra);
+
+NSIS_API void StartInstall(HWND hWndParent, int nStringSize, LPTSTR lpszVariables, stack_t** pStackTop, extra_parameters *extra);
+NSIS_API void StartUninstall(HWND hWndParent, int nStringSize, LPTSTR lpszVariables, stack_t** pStackTop, extra_parameters *extra);
